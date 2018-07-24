ae7e289e779ffcb582b05ed8e1a37a5a142a6085
jonathanreedmevs <jrmevs@umd.edu>
2018-07-24 11:29:37 -0400

    update

--- hotSeats.js
+++ hotSeats.js
@@ -1,11 +1,11 @@
 let students = ["Garrett", "Andy", "Ronaldo", "Alieu", "Sage", "Bryan", "Oscar",
-                "Leron", "Sammy", "Christian", "Gabriel", "Saif", "Pablo", "El-hassn", "Jeffrey"]
+                "Leron", "Sammy", "Christian", "Gabriel", "Saif", "Jeffrey", "Jonathon"]
 
 
-for(let i = 0; i < 4; i++){
+for(let i = 0; i < 1; i++){
     let randomNumber = Math.floor(Math.random() * students.length);
     let randomStudent = students[randomNumber]; 
     console.log(randomStudent);
     students.splice(randomNumber, 1);
 }
-console.log(students.length);
+
