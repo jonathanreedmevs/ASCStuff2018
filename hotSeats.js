57dbd968c8768c885fb04c660cfc867aaa56f5b4
jonathanreedmevs <jrmevs@umd.edu>
2018-07-27 15:42:34 -0400

    failed to commit

--- hotSeats.js
+++ hotSeats.js
@@ -1,8 +1,8 @@
-let students = ["Garrett", "Andy", "Ronaldo", "Alieu", "Sage", "Bryan", "Oscar",
-                "Leron", "Sammy", "Christian", "Gabriel", "Saif", "Jeffrey", "Jonathon"]
+let students = ["Garrett", "Ronaldo", "Alieu", "Sage", "Bryan", "Oscar",
+                "Leron", "Sammy", "Christian", "Gabriel", "Saif", "Jeffrey", "Jonathon", "El-hassn"]
 
 
-for(let i = 0; i < 1; i++){
+for(let i = 0; i < 4; i++){
     let randomNumber = Math.floor(Math.random() * students.length);
     let randomStudent = students[randomNumber]; 
     console.log(randomStudent);
